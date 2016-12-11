529b0c51ffa757e78b5770423223037cbad7514b
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2016-12-10 19:28:56 -0500

    Add some new aliases

--- zsh/.zsh/custom/aliases.zsh
+++ zsh/.zsh/custom/aliases.zsh
@@ -1,2 +1,10 @@
 # Find a random TODO to fix
 alias fixtodo='vim $(ag -l TODO | gshuf | head -n 1)'
+
+alias :q="echo This isn\'t vim, asshole"
+alias :wq=:q
+
+alias saver='open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'
+
+# Lookup information on an IP
+function ipinfo() { curl "ipinfo.io/$1" }
