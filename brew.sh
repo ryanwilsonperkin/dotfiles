ee138aa3b9631e11c848c8a93686006858cbefb5
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2018-02-01 20:08:22 -0500

    Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+brew tap "caskroom/cask"
+brew tap "neovim/neovim"
+
+brew cask install docker
+brew cask install karabiner
+brew cask install macdown
+
+brew install ab
+brew install colordiff
+brew install fish
+brew install git
+brew install go
+brew install htop
+brew install httpie
+brew install hub
+brew install imagemagick
+brew install jq
+brew install neovim
+brew install node
+brew install python3
+brew install stow
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install tree
+brew install yarn
