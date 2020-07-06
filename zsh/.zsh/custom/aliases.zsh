b2bbcccc890f9d33dc53efa46840b4833663febc
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-07-06 10:31:04 -0400

    Update zsh aliases

--- zsh/.zsh/custom/aliases.zsh
+++ zsh/.zsh/custom/aliases.zsh
@@ -1,16 +1,11 @@
-# Find a random TODO to fix
-alias fixtodo='vim $(ag -l TODO | gshuf | head -n 1)'
-
+# Aliases
 alias vim=nvim
-alias :q="echo This isn\'t vim, asshole"
-alias :wq=:q
 
-alias saver='open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'
-
+# Functions
 # Lookup information on an IP
 function ipinfo() { curl "ipinfo.io/$1" }
 
 # Tmux
-alias ts='tmux new-session -s'
+alias tn='tmux new-session -s'
 alias ta='tmux attach -t'
 alias tl='tmux list-sessions'
