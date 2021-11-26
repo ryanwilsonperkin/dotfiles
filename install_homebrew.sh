9011253543c39e998e3588ac1657865acf93adad
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-11-26 12:58:33 -0500

    Update install homebrew script

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
