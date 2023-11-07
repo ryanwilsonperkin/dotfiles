89a14062eacde462ffd87a9438dbf7251892bd07
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-11-07 09:44:16 -0500

    More aliases

--- zsh/.zsh/custom/aliases.zsh
+++ zsh/.zsh/custom/aliases.zsh
@@ -11,17 +11,21 @@ alias gco="git checkout"
 alias gcd="git checkout-default"
 alias gc="git commit --verbose"
 alias gca="git commit --verbose --all"
+alias gcm="git commit -m"
 alias gc!="gc --amend"
 alias gca!="gca --amend"
 alias gp="git pull"
 alias grb="git rebase"
 alias grbi="git rebase --interactive"
+alias grc="git rebase --continue"
+alias grr="git rm -r"
 alias ls="ls -G"
 alias ll="ls -l"
 alias la="ls -la"
 alias k=kubectl
 alias zupdate="source ~/.zshrc"
 alias pdfcat='gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=-'
+alias ag='ag --hidden'
 
 # Functions
 # Lookup information on an IP
@@ -30,8 +34,11 @@ function ipinfo() { curl "ipinfo.io/$1" }
 function edit() { vim $(ag -l $@) }
 # Edit files that match in VS Code
 function vsedit() { code $(ag -l $@) }
+function ci() { open "https://buildkite.com/shopify/${1:-web-ci-builder}/builds?branch=$(git rev-parse --abbrev-ref HEAD)" }
 
 # Tmux
 alias tn='tmux new-session -s'
 alias ta='tmux attach -t'
 alias tl='tmux list-sessions'
+
+alias s="noti sleep"
