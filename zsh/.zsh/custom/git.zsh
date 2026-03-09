0bc7a873b924634a439651a11dbb54f1d213d517
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2026-03-08 21:12:27 -0400

    chore(zsh): remove unused git aliases

--- zsh/.zsh/custom/git.zsh
+++ zsh/.zsh/custom/git.zsh
@@ -7,7 +7,6 @@ alias gr="git rebase"
 alias gd="git diff"
 alias gb="git branch --sort=-committerdate"
 alias gco="git checkout"
-alias gcd="git checkout-default"
 alias gc="git commit --verbose"
 alias gca="git commit --verbose --all"
 alias gcm="git commit -m"
@@ -17,8 +16,6 @@ alias gp="git pull"
 alias grb="git rebase"
 alias grbi="git rebase --interactive"
 alias grc="git rebase --continue"
-alias grr="git rm -r"
-alias gw="git worktree"
 
 # Git functions
 function gmb() {git merge-base origin/main HEAD}
